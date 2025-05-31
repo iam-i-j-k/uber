0d9c2645df4c7410a524438bdf7c73cede94c406
iam-i-j-k <irfanjankhan7860@gmail.com>
2025-05-31 20:14:10 +0530

    cors

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ const captainRoutes = require('./routes/captain.routes');
 
 connectToDb();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
